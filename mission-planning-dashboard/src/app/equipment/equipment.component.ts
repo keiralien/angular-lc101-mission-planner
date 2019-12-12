0a74509c570a5f48b99a006cd9759b87efe64e01
keiralien <jen.murphy8200@gmail.com>
2019-12-11 20:22:43 -0600

    Part 1 Studio complete

---
+++ mission-planning-dashboard/src/app/equipment/equipment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment',
+  templateUrl: './equipment.component.html',
+  styleUrls: ['./equipment.component.css']
+})
+export class EquipmentComponent implements OnInit {
+  equipment: string[] = ['Habitat Dome', 'Drones', 'Food Containers', 'Oxygen Tanks'];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
